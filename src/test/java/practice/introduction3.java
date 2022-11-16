2125cb7bdfc9f9d3decec15eac074c33cc140638
aswini <aswinithumala@gmail.com>
2022-11-15 22:51:00 -0800

    aswini

---
+++ src/test/java/practice/introduction3.java
@@ -0,0 +1,29 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class introduction3 {
+	@Test
+	public void javaprogram()
+	{
+		System.out.println("hello");
+		System.out.println("HIIIII");
+		System.out.println("ASWINIHIIII");
+	}
+	@Test
+	public void testng()
+	{
+		System.out.println("test");
+		System.out.println("java");
+		System.out.println("java");
+		
+	}
+	@Test
+	public void aswini()
+	{
+		System.out.println("test");
+		System.out.println("java");
+		System.out.println("java");
+	}
+
+}
